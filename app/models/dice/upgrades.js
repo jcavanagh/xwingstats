9ac16dc6b697088aba04a091e86a4842f6eaf153
Joe Cavanagh <jcavanagh@apple.com>
2016-02-12 15:51:19 -0800

    Adding sensor jammer and emperor upgrades

--- app/models/dice/upgrades.js
+++ app/models/dice/upgrades.js
@@ -1,42 +1,68 @@
 import meld from 'meld';
 
+//Series rotater
+function smushRight(series) {
+	//Zero- or one-length series are not valid
+	if(!series || !series.length || series.length == 1) { return ; }
+
+	//Rotate right 1
+	series.unshift(series.pop());
+
+	///Combine best and rotated second best
+	var last = series.length - 1;
+	series[last] = series[0] + series[last];
+
+	//Zero out zero
+	series[0] = 0;
+}
+
+function smushLeft(series) {
+	//Zero- or one-length series are not valid
+	if(!series || !series.length || series.length == 1) { return ; }
+
+	//Rotate left 1
+	series.push(series.shift());
+
+	///Combine worst and rotated second worst
+	var last = series.length - 1;
+	series[last] = series[0] + series[last];
+
+	//Zero out last
+	series[last] = 0;
+}
+
 export var sensor_jammer = {
 	priority: 1,
 	fn: function(scope) {
-		//NYI
+		return meld.around(scope, [ 'getHitOrCritSeries', 'getHitSeries'], function(joinpoint) {
+			var attacker = joinpoint.target.attacker;
+			var defender = joinpoint.target.defender;
+			var series = joinpoint.proceed();
+
+			//Sensor jammer effectively removes an attack die if the attacker is not focused
+			if(defender.getUpgrade('sensor_jammer') && !attacker.focus) {
+				smushLeft(series);
+			}
+
+			return series;
+		});
 	}
 }
 
 export var emperor_palpatine = {
 	priority: 99,
 	fn: function(scope) {
-		//Series rotater
-		function smushRight(series) {
-			//Zero- or one-length series are not valid
-			if(!series || !series.length || series.length == 1) { return ; }
-
-			//Rotate right 1
-			series.unshift(series.pop());
-
-			///Combine best and rotated second best
-			var last = series.length - 1;
-			series[last] = series[0] + series[last];
-
-			//Zero out zero
-			series[0] = 0;
-		}
-
 		return meld.around(scope, [ 'getHitOrCritSeries', 'getCritSeries', 'getEvadeSeries'], function(joinpoint) {
 			var attacker = joinpoint.target.attacker;
 			var defender = joinpoint.target.defender;
 			var series = joinpoint.proceed();
 
-			//The Emperor guarantees at least one crit/evade, so the entire distribution shifts right
+			//The Emperor guarantees at least one crit/evade, so the entire distribution shifts
 			if(joinpoint.method === 'getEvadeSeries') {
 				if(defender.getUpgrade('emperor_palpatine')) {
 					smushRight(series);
 				}
-			} else {
+			} else if(joinpoint.method === 'getCritSeries' || joinpoint.method === 'getHitOrCritSeries') {
 				if(attacker.getUpgrade('emperor_palpatine')) {
 					smushRight(series);
 				}
